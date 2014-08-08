b792c21a06a36757e4c1180fcee5378a3ac520cd
Mike Sperber <sperber@deinprogramm.de>
2014-08-08 13:33:13 +0200

    Prepare for CLJX compilation.
    I.e. only the necessary additions to project.clj and moving files around.

--- project.clj
+++ project.clj
@@ -3,4 +3,34 @@
   :url "http://github.com/active-group/reacl/zippers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2280" :scope "provided"]]
+
+  :plugins [[com.keminglabs/cljx "0.4.0"]
+            [lein-cljsbuild "1.0.3"]
+            [org.bodil/lein-nashorn "0.1.2"]]
+
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                   :output-path "target/generated/src/clj"
+                   :rules :clj}
+                  
+                  {:source-paths ["src/cljx"]
+                   :output-path "target/generated/src/cljs"
+                   :rules :cljs}
+
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/generated/test/clj"
+                   :rules :clj}
+                  
+                  {:source-paths ["test/cljx"]
+                   :output-path "target/generated/test/cljs"
+                   :rules :cljs}]}
+
+  :test-paths ["target/generated/test/clj"]
+
+  :cljsbuild {:builds {:dev {:source-paths ["target/classes"]
+                             :compiler {:output-to "target/main.js"
+                                        :optimizations :whitespace
+                                        :pretty-print true}}}}
+
+  :hooks [cljx.hooks])
